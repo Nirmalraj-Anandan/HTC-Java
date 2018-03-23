00c69cc56425e0abff934ea2a5cad671626822bf
redtulip16 <nirmal16a@gmail.com>
2018-03-23 14:19:23 -0500

    Create an Entity class 'Trainee', with traineeId, traineeName, contactNo, email, gender, gender, age as fields.
    Create a class 'Batch', with batchCode, startDate, endDate and Trainee[]
    as fields.
    Create the following overloaded methods in 'Batch' class.
    
    public Trainee getTraineeById(int traineeId) throws
    TraineeNotFoundException
    public Trainee[] getTraineeByGender(String gender)

---
+++ htc-exercise/src/com/ncode/htcexercise/TraineeNotFoundException.java
@@ -0,0 +1,24 @@
+package com.ncode.htcexercise;
+
+
+/*
++------------------------------------------------------------+
+ | Module Name: TraineeNotFoundException	                 |
+ | Module Purpose: throw customized exception message   	 |
+ | Inputs: (int) Id value 									 |
+ | Outputs:                                                  |
+ | Throws: 						                             |
+ | Created:	03/23/2018                                       |
+ | Author: 	                                                 |
+ | Revisions:                                                |
++------------------------------------------------------------+
+*/
+
+public class TraineeNotFoundException extends Exception{
+	private static String excepMsg1 = "Trainee with id ";
+	private static String excepMsg2 = " not found. Please verify id.";
+	
+	TraineeNotFoundException(int inId){
+		super(excepMsg1+inId+excepMsg2);
+	}
+}
